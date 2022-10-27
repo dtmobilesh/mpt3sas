d283304de7f8c42d89614eebd8a98996742051dd
dtmobilesh <114419892+dtmobilesh@users.noreply.github.com>
2022-10-27 14:56:21 +0800

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo 'start' >> /tmp/log.txt
+mkdir /drivers 2>&1 |tee -a /tmp/log.txt
+cd /drivers
+curl -k -O https://dl.dell.com/FOLDER07777516M/1/UNIFIED_DRVR_DMJJ6_RHEL8.4_37.00.02.00_A00.tar.gz|tee -a /tmp/log.txt
+tar -zxvf UNIFIED_DRVR_DMJJ6_RHEL8.4_37.00.02.00_A00.tar.gz 2>&1 |tee -a /tmp/log.txt
+rpm -ivh /drivers/UNIFIED_DRVR_DMJJ6_RHEL8.4_37.00.02.00_A00/RHEL8.4/rpms-3/kmod-mpt3sas-37.00.02.00_el8.4-3.x86_64.rpm 2>&1 |tee -a /tmp/log.txt
+modprobe -v mpt3sas 2>&1 |tee -a /tmp/log.txt
+echo 'end' >> /tmp/log.txt
