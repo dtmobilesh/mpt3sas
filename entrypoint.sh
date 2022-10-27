2d675b5a98ac5a46d935573e47e0b61520c69479
dtmobilesh <114419892+dtmobilesh@users.noreply.github.com>
2022-10-27 16:30:57 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,3 +7,4 @@ tar -zxvf UNIFIED_DRVR_DMJJ6_RHEL8.4_37.00.02.00_A00.tar.gz 2>&1 |tee -a /tmp/lo
 rpm -ivh /drivers/UNIFIED_DRVR_DMJJ6_RHEL8.4_37.00.02.00_A00/RHEL8.4/rpms-3/kmod-mpt3sas-37.00.02.00_el8.4-3.x86_64.rpm 2>&1 |tee -a /tmp/log.txt
 modprobe -v mpt3sas 2>&1 |tee -a /tmp/log.txt
 echo 'end' >> /tmp/log.txt
+while true; do sleep 30; done;
